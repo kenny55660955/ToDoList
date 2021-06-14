528d97657ee674c2b0e7e40a91a1cda8129e137a
Kenny <kenny55660955@gmail.com>
2021-06-14 15:46:53 +0800

    feat: Add

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  Destini
+//  Todoey
 //
-//  Created by Philipp Muellauer on 01/09/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//  Created by Angela Yu on 16/11/2017.
+//  Copyright © 2017 Angela Yu. All rights reserved.
 //
 
 import UIKit
@@ -17,41 +17,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
-        
         return true
     }
-    
+
+
     func applicationWillTerminate(_ application: UIApplication) {
+
         self.saveContext()
     }
-
+    
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
-        
+
         let container = NSPersistentContainer(name: "DataModel")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
+
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         return container
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
                 try context.save()
             } catch {
+
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
     }
+
 
 
 }
